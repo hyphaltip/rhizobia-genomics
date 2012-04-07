4a8d06a4b4f080053fd3b7ef06eb22dacab29b9a
Jason Stajich <jason.stajich@ucr.edu>
2012-04-07 19:19:31 -0400

    add quiet flag

--- scripts/diginorm_reads.pl
+++ scripts/diginorm_reads.pl
@@ -20,7 +20,7 @@ module load stajichlab-python
 EOF
 ;
 $dir = File::Spec->rel2abs($dir);
-opendir(DIR, $dir)|| die $!;
+opendir(DIR, $dir)|| die "$dir: $!";
 
 for my $file ( readdir(DIR) ) {
   if ( $file =~ /(\S+)\.fq(\.gz)?/) {
@@ -28,9 +28,9 @@ for my $file ( readdir(DIR) ) {
     open(my $jobfh => ">$jobdir/$strain.diginorm.sh") || die $!;
     print $jobfh $job_header;
     print $jobfh "cd $dir\n";
-    printf $jobfh "normalize-by-median.py -C 20 -k 20 -N 4 -x 8e8 --savehash $strain.kh $file\n";
+    printf $jobfh "normalize-by-median.py -q -C 20 -k 20 -N 4 -x 8e8 --savehash $strain.kh $file\n";
     printf $jobfh "filter-abund.py $strain.kh $file.keep\n";
-    printf $jobfh "normalize-by-median.py -C 5 -k 20 -N 4 -x 4e8 $file.keep.abundfilt\n";
+    printf $jobfh "normalize-by-median.py -q -C 5 -k 20 -N 4 -x 4e8 $file.keep.abundfilt\n";
     printf $jobfh "python \$KHMERROOT/sandbox/strip-and-split-for-assembly.py $file.keep.abundfilt.keep\n";
     close($jobfh);
   }
