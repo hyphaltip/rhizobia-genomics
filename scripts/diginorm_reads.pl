a2253638483ab887637902fe2359a0090b0a1217
Jason Stajich <jason.stajich@ucr.edu>
2012-04-07 18:36:15 -0400

    diginorm reads update

---
+++ scripts/diginorm_reads.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl -w
+use strict;
+use Getopt::Long;
+use File::Spec;
+
+my $jobdir = 'jobs';
+
+GetOptions(
+	   'j|jobdir:s' => \$jobdir,
+	   );
+
+my $dir = shift || die "no dir";
+
+mkdir($jobdir) unless -d $jobdir;
+my $job_header = <<EOF
+#PBS -N diginorm -l nodes=1:ppn=1 -q highmem
+module load stajichlab
+module load khmer
+module load stajichlab-python
+EOF
+;
+$dir = File::Spec->rel2abs($dir);
+opendir(DIR, $dir)|| die $!;
+
+for my $file ( readdir(DIR) ) {
+  if ( $file =~ /(\S+)\.fq(\.gz)?/) {
+    my $strain = $1;
+    open(my $jobfh => ">$jobdir/$strain.diginorm.sh") || die $!;
+    print $jobfh $job_header;
+    print $jobfh "cd $dir\n";
+    printf $jobfh "normalize-by-median.py -C 20 -k 20 -N 4 -x 8e8 --savehash $strain.kh $file\n";
+    printf $jobfh "filter-abund.py $strain.kh $file.keep\n";
+    printf $jobfh "normalize-by-median.py -C 5 -k 20 -N 4 -x 4e8 $file.keep.abundfilt\n";
+    printf $jobfh "python \$KHMERROOT/sandbox/strip-and-split-for-assembly.py $file.keep.abundfilt.keep\n";
+    close($jobfh);
+  }
+}
