a39ad2d234a823b5ff891fb3b3cd9b2f3bf44d7f
Jason Stajich <jason.stajich@ucr.edu>
2012-04-07 18:01:28 -0400

    combine reads to one file

--- scripts/combine_readfiles.pl
+++ scripts/combine_readfiles.pl
@@ -9,21 +9,30 @@ my $job_header = <<EOF
 #PBS -N combineReads -l nodes=1:ppn=1
 module load stajichlab
 module load velvet
-module load rhizobia-bacteria
+module load rhizobia-genomics
 EOF
 ;
 my $add_fwdrev_to_readid = 0; # add /1 and /2 to read ids
+my $reads_dir = 'reads';
 GetOptions(
 	   'hdr:s'       => \$job_header,
 	   'd|dir:s'     => \$reads_dir,
 	   's|strains:s' => \$strain_file,
 	   'column:i'    => \$strain_column,
-	   'add|addfwd!' => \$add_fwdrev_to_readod,
+	   'add|addfwd!' => \$add_fwdrev_to_readid,
 );
+if ( ! defined $reads_dir || ! -d $reads_dir ) {
+  warn("no read directory provided, using current directory\n");
+  $reads_dir = ".";
+}
 
-open(my $fh => $strains) || die $!;
+if ( ! -f $strain_file ) {
+  die("must have a strain file to insure we are picking up proper strains\n");
+}
+
+open(my $fh => $strain_file) || die "cannot open strainfile '$strain_file': $!";
 my %strains;
-while (<$strains>) {
+while (<$fh>) {
   my @row = split;
   $strains{$row[$strain_column]}++; # grab strain name from specific column
 }
@@ -44,7 +53,8 @@ for my $file (readdir(DIR) ) {
       if ( $rest =~ /(\S+)_trim_(\S+)/) {
 	my ($strain,$direction) = ($1,$2);
 	if ( ! exists $strains{$strain} )  {
-	  warn("unknown strain $strain, is the parser working?\n");
+	  warn("unknown strain $strain, is the parser working properly?\n");
+	  next;
 	} else {
 	  $strains{$strain}->{$fc."_".$lane}->{$direction} = $file;
 	}
@@ -57,29 +67,28 @@ for my $file (readdir(DIR) ) {
   }
 }
 
-
 for my $strain ( sort keys %strains) {
   my @ofiles;
   open(my $jobfh => ">$strain.combine.sh") || die $!;
-  print $jobfh $jobheader;
-  for my $lane ( keys %{$strain{$strain}} ) {
+  print $jobfh $job_header;
+  for my $lane ( keys %{$strains{$strain}} ) {
     my $ofile = sprintf("%s.fq",$lane);
     if ( exists $strains{$strain}->{$lane}->{1} &&
 	 exists $strains{$strain}->{$lane}->{2} ) {
-      printf ( "shuffleSequences_fastq.pl %s %s %s\n",
+      printf $jobfh ( "shuffleSequences_fastq.pl %s %s %s\n",
 	       $strains{$strain}->{$lane}->{1},
 	       $strains{$strain}->{$lane}->{2},
 	       $ofile);
       if ( $add_fwdrev_to_readid ) {
-	printf "add_fwdrev_nums_pairedfq.pl $ofile > $ofile.update\n";
-	printf "$ofile $ofile.backup # you can remove all the .backup when sure this worked\n";
-	printf "mv $ofile.update $ofile\n";
+	printf $jobfh "add_fwdrev_nums_pairedfq.pl $ofile > $ofile.update\n";
+	printf $jobfh "$ofile $ofile.backup # you can remove all the .backup when sure this worked\n";
+	printf $jobfh "mv $ofile.update $ofile\n";
       }
     }
     if ( exists $strains{$strain}->{$lane}->{single} ) {
-      printf "cat %s >> %s\n",$strains{$strain}->{$lane}->{single},$ofile;
+      printf $jobfh "cat %s >> %s\n",$strains{$strain}->{$lane}->{single},$ofile;
     }
     push @ofiles, $ofile;
   }
-  printf "cat %s >> $strain.fq", join(" ", @ofiles);
+  printf $jobfh "cat %s >> $strain.fq", join(" ", @ofiles);
 }
