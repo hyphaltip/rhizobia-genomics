acad8607633c756a0c515282f7e376593fe1c4ca
Jason Stajich <jason.stajich@ucr.edu>
2012-04-09 23:03:21 -0700

    make velvet jobs

---
+++ scripts/make_velvet_jobs.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl -w
+use strict;
+use Getopt::Long;
+use File::Spec;
+my $jobdir = 'jobs';
+my $queue = 'js';
+my $min = 17;
+my $max = 71;
+GetOptions(
+	   'j|jobs:s' => \$jobdir,
+	   'min:i'    => \$min,
+	   'max:i'    => \$max,
+	   'q|queue:s'=> \$queue,
+	  );
+
+my $dir = shift || die "need a directory";
+$dir = File::Spec->rel2abs($dir);
+opendir(INDIR,$dir) || die $!;
+my %strains;
+for my $file (readdir(INDIR) ) {
+  if ( $file =~ /(\S+)\.fq(?:.gz)?\.keep\.abundfilt\.keep\.(se|pe)/) {
+    my ($strain,$kind) = ($1,$2);
+    mkdir($strain) unless -d $strain;
+    warn "file is $file, strains is $strain, kind is $kind\n";
+    $strains{$strain}->{$kind} = File::Spec->catfile($dir,$file);
+  }
+}
+for my $strain ( keys %strains ) {
+  open(my $jobfh => ">$jobdir/$strain.velveth.sh") || die $!;
+  my $files;
+  if ( exists $strains{$strain}->{pe} ) {
+    $files = sprintf("-fasta -shortPaired %s",$strains{$strain}->{pe});
+  }
+  if ( exists $strains{$strain}->{se} ) {
+    $files .= sprintf(" -fasta -short %s",$strains{$strain}->{se});
+  }
+  print $jobfh <<EOF
+#PBS -N $strain.velh -l nodes=1:ppn=8,mem=36gb -q $queue
+#PBS -j oe
+module load stajichlab
+module load velvet
+cd $dir/$strain
+velveth asm $min,$max,2 -create_binary $files
+EOF
+;
+  open($jobfh => ">$jobdir/$strain.velvetg.sh") || die $!;
+  print $jobfh <<EOF
+#PBS -N $strain.velg -l nodes=1:ppn=8,mem=36gb -q $queue
+#PBS -j oe
+module load stajichlab
+module load velvet
+cd $dir/$strain
+for asmdir in asm_??
+do
+ velvetg \$asmdir -exp_cov auto -cov_cutoff auto -ins_length 350 -amos_file yes -min_contig_lgth 500
+ done
+
+EOF
+    ;
+}
