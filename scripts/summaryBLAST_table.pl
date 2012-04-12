adb15f530f6621970b235d7924cd6442f036a1cb
Jason Stajich <jason.stajich@ucr.edu>
2012-04-12 00:37:30 -0700

    only keep the best %ID

--- scripts/summaryBLAST_table.pl
+++ scripts/summaryBLAST_table.pl
@@ -21,7 +21,15 @@ for my $f ( sort readdir(DIR) ) {
     while( my $hit = $result->next_hit ) {
       while( my $hsp = $hit->next_hsp ) {
 	if ( $hsp->percent_identity >= 50 ) {
-	  # store the %ID and convert this to only 2 FP decimals to make formatting easier
+	  # store the %ID and convert this to only 2 FP decimals 
+	  # to make formatting easier
+	  if ( exists $table{$result->query_name}->{$strain} )  {
+	    # already saw an HSP, we want the one with the BEST %ID for now
+	    # so compare the two
+	    next if $table{$result->query_name}->{$strain} > $hsp->percent_identity;
+	    # if the value of the current stored is greater than the one we are reading
+	    # we don't want to update the value
+	  }
 	  $table{$result->query_name}->{$strain} = sprintf("%.2f", $hsp->percent_identity);
 	}
       }
